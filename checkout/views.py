20e7d56b80b8394757eb715c461f02e529317c85
monkphin <darren.burrows@gmail.com>
2025-03-10 20:48:59 +0000

    profiles part 4 - added order detailsd

--- checkout/views.py
+++ checkout/views.py
@@ -121,8 +121,6 @@ def checkout(request):
         else:
             order_form = OrderForm()
 
-        order_form = OrderForm()
-
     if not stripe_public_key:
         messages.warning(request, 'Stripe public key is missing. \
             Did you forget to set it in your environment?')
--- profiles/views.py
+++ profiles/views.py
@@ -29,14 +29,15 @@ def profile(request):
     return render(request, template, context)
 
 def order_history(request, order_number):
-    order = get_object_or_404(order, order_number=order_number)
+    order = get_object_or_404(Order, order_number=order_number)
 
     messages.info(request, (
         f'This is a past confirmation for order number {order_number}'
         'A confirmation email was sent on the order date'
     ))
 
-    template = 'cheeckout/checkout_success.html'
+    template = 'checkout/checkout_success.html'
+
     context = {
         'order': order,
         'from_profile': True
