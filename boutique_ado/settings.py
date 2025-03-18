a1d2caa05dd4ad6ebb9c7ce9263df041154aff36
monkphin <darren.burrows@gmail.com>
2025-03-18 20:52:00 +0000

    cleaned up settings to enhance security

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -25,10 +25,10 @@ load_dotenv(os.path.join(BASE_DIR, '.env'))
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-2&fau63*0ra4@mt9@_8u@+#=h1i$7kpxy(v$w^&%vjb10fu+y+')
+SECRET_KEY = os.getenv.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DEBUG', 'False') == 'True'
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['darrenb-boutique-ado-62c8bdadb51d.herokuapp.com', 'localhost', '127.0.0.1']
 
