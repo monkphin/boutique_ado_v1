36a33448b3b9f4e2db3862ef5ef95b030f142dc6
monkphin <darren.burrows@gmail.com>
2025-03-18 20:39:15 +0000

    cleaning up delployment

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -122,11 +122,12 @@ WSGI_APPLICATION = 'boutique_ado.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+
 if 'DATABASE_URL' in os.environ:
     DATABASES = {
-        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+        'default': dj_database_url.parse('DATABASE_URL')
     }
-else: 
+else:
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
